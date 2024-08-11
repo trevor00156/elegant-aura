76e4ac9528909b65120b82baa226f3f876c1eec0
trevor00156 <kathuriarohan22@gmail.com>
2024-08-11 22:03:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        var menuItems = document.getElementById('menu-items');
+        if (menuItems.style.display === 'block') {
+            menuItems.style.display = 'none';
+        } else {
+            menuItems.style.display = 'block';
+        }
+    });
+</script>
+
+
+
+
+cansole.log("Hello world")
